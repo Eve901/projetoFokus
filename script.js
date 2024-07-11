b68f38ffb9cddb994d06915688188fa960f50336
Evellin LF <evellin.lima.901@gmail.com>
2024-07-11 00:10:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const html = document.querySelector('html');
+const focoBotao = document.querySelector('.app__card-button--foco')
+const curtoBotao = document.querySelector('.app__card-button--curto')
+const longoBotao = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicafocoinput = document.querySelector('#alternar-musica')
+const musica = new Audio('./sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+musicafocoinput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
+
+focoBotao.addEventListener('click', () =>{
+    alterarContexto('foco')
+    focoBotao.classList.add('active')
+})
+
+curtoBotao.addEventListener('click', () =>{
+    alterarContexto('descanso-curto')
+    curtoBotao.classList.add('active')
+})
+
+longoBotao.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
+    longoBotao.classList.add('active')
+})
+
+function alterarContexto(contexto) {
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco': 
+            titulo.innerHTML = `
+                Otimize sua produtividade, <br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+                `
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `
+                Que tal dar uma respirada? <br>
+                <strong class="app__title-strong">Faça uma pausa curta.</strong>
+                `
+            break;
+
+        case 'descanso-longo':
+            titulo.innerHTML = `
+                Hora de voltar à superfície. <br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+                `
+        default:
+            break;
+        
+    }
+}
+
